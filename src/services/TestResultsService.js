f53c3b62617ad63733de29b37bc6d3b1d0f65ab6
oalexandru <oalexandru@deloittece.com>
2019-02-07 11:57:46 +0200

    Removed unused code

--- src/services/TestResultsService.js
+++ src/services/TestResultsService.js
@@ -83,63 +83,6 @@ class TestResultsService {
       })
   }
 
-  setTestCode (payload) {
-    var vehicleType = payload.vehicleType
-    var vehicleSize = payload.vehicleSize
-    var vehicleConfiguration = payload.vehicleConfiguration
-    var fields = 'defaultTestCode,linkedTestCode'
-    if (payload.testTypes.length === 0) {
-      return Promise.reject(new HTTPError(400, 'Bad request'))
-    } else if (payload.testTypes.length === 1) {
-      var testTypeId = payload.testTypes[0].testId
-      let options = {
-        uri: `${config.TEST_TYPES_ENDPOINT}/${testTypeId}?vehicleType=${vehicleType}&vehicleSize=${vehicleSize}&vehicleConfiguration=${vehicleConfiguration}&fields=${fields}`,
-        json: true,
-        port: 3006
-      }
-
-      return rp(options).then(testCodeResponse => {
-        payload.testTypes[0].testCode = testCodeResponse.defaultTestCode
-        return payload
-      }).catch(err => {
-        console.error(err)
-        throw new HTTPError(500, 'Internal Server Error')
-      })
-    } else {
-      var promiseArray = []
-      var testCodes = []
-      for (let i = 0; i < payload.testTypes.length; i++) {
-        testTypeId = payload.testTypes[i].testId
-
-        let options = {
-          uri: `${config.TEST_TYPES_ENDPOINT}/${testTypeId}?vehicleType=${vehicleType}&vehicleSize=${vehicleSize}&vehicleConfiguration=${vehicleConfiguration}&fields=${fields}`,
-          json: true,
-          port: 3006
-        }
-
-        const promise = rp(options).then(testCodeResponse => {
-          testCodes.push(testCodeResponse)
-        }).catch((err) => {
-          console.error(err)
-          throw new HTTPError(500, 'Internal Server Error')
-        })
-        promiseArray.push(promise)
-      }
-
-      return Promise.all(promiseArray).then(() => {
-        for (let i = 0; i < payload.testTypes.length; i++) {
-          if (testCodes[i].linkedTestCode) {
-            payload.testTypes[i].testCode = testCodes[i].linkedTestCode
-          } else {
-            payload.testTypes[i].testCode = testCodes[i].defaultTestCode
-          }
-        }
-      }).then(() => {
-        return payload
-      })
-    }
-  }
-
   insertTestResultsList (testResultsItems) {
     return this.testResultsDAO.createMultiple(testResultsItems)
       .then(data => {
