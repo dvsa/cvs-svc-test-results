f32003dd7ee5315f46a0faf14b1a354721f37c3f
Nathan Blore <37145422+nblore@users.noreply.github.com>
2019-06-06 13:12:23 +0100

    Revert "added prohibitionIssued for defects"

--- src/models/TestResultsSchemaSubmitted.js
+++ src/models/TestResultsSchemaSubmitted.js
@@ -23,7 +23,6 @@ const defectsSchema = Joi.object().keys({
   deficiencyText: Joi.string().required().allow('', null),
   deficiencyCategory: Joi.any().only(['advisory', 'dangerous', 'major', 'minor']).required(),
   stdForProhibition: Joi.boolean().required().allow(null),
-  prohibitionIssued: Joi.boolean().required().allow(null),
   prs: Joi.boolean().required().allow(null)
 })
 
