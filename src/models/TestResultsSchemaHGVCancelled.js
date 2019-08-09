a32ca0ed90c53bf67cb14d87f442484310e69074
oalexandru <oalexandru@deloittece.com>
2019-08-09 15:14:00 +0300

    Fixed a bug

--- src/models/TestResultsSchemaHGVCancelled.js
+++ src/models/TestResultsSchemaHGVCancelled.js
@@ -29,6 +29,8 @@ const testTypesSchema = Joi.object().keys({
 const testResultsSchema = Joi.object().keys({
   ...testResultsCommonSchema,
   vrm: Joi.string().alphanum().min(1).max(8).required(),
+  odometerReading: Joi.number().required().allow(null),
+  odometerReadingUnits: Joi.any().only(['kilometres', 'miles']).required().allow(null),
   reasonForCancellation: Joi.string().max(500).required().allow(''),
   euVehicleCategory: Joi.any().only(['m1', 'm2', 'm3', 'n1', 'n2', 'n3', 'o1', 'o2', 'o3', 'o4']).required().allow(null),
   countryOfRegistration: Joi.string().required().allow('', null),
