71e01acae321d7153eafda0efe3af1b4530b4fbc
Arup Banerjee <arupdvsa@alanaamy.net>
2019-09-03 12:57:53 +0100

    added type ISubSeg

--- src/functions/getTestResultsByTesterStaffId.ts
+++ src/functions/getTestResultsByTesterStaffId.ts
@@ -1,6 +1,7 @@
 import {TestResultsDAO} from "../models/TestResultsDAO";
 import {TestResultsService} from "../services/TestResultsService";
 import {HTTPResponse} from "../models/HTTPResponse";
+import { ISubSeg } from "./ISubSeg";
 /* workaround AWSXRay.captureAWS(...) call obscures types provided by the AWS sdk.
 https://github.com/aws/aws-xray-sdk-node/issues/14
 */
@@ -11,7 +12,7 @@ const AWSXRay = require('aws-xray-sdk');
 const getTestResultsByTesterStaffId = async (event: any) => {
   const segment = AWSXRay.getSegment();
   AWSXRay.capturePromise();
-  let subseg: { addError: { (arg0: string): void; (arg0: { statusCode: any; body: any; }): void; }; close: () => void; } | null = null;
+  let subseg: ISubSeg | null = null;
   if (segment) {
     subseg = segment.addNewSubsegment("getTestResultsByTesterStaffId");
   }
