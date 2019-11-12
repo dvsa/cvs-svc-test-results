254f7b4ea92b1c50792c7b2e6915914bb9559f8e
khodder <khodder@gmail.com>
2019-11-12 16:01:20 +0000

    CVSB-7523 - additional tests

--- tests/unit/testResultsService.unitTest.ts
+++ tests/unit/testResultsService.unitTest.ts
@@ -1,5 +1,6 @@
 import {TestResultsService} from "../../src/services/TestResultsService";
 import {TEST_RESULT, TEST_STATUS, TEST_TYPE_CLASSIFICATION} from "../../src/assets/Enums";
+import {cloneDeep} from "lodash";
 
 describe("Test Results Service", () => {
   describe("generateCertificateNumber", () => {
@@ -46,6 +47,68 @@ describe("Test Results Service", () => {
           const output = srv.generateCertificateNumber(myPayload);
           expect(output.testTypes[0].certificateNumber).toEqual("originalCertNumber");
         });
+      });
+    });
+  });
+  describe("addMissingRequiredCertificateNumberOnLec", () => {
+    describe("with LEC test type", () => {
+      describe("with No certificateNumber field", () => {
+        it("should modify the input to add a certificateNumber field", () => {
+          // @ts-ignore
+          const srv = new TestResultsService(null);
+
+          const myPayload: any = {
+            testStatus: TEST_STATUS.SUBMITTED,
+            testTypes: [
+              {
+                testTypeId: "39", // an LEC test type
+              }
+            ]
+          };
+          const expectedToNotMatch = cloneDeep(myPayload);
+
+          srv.addMissingRequiredCertificateNumberOnLec(myPayload);
+          expect(myPayload).not.toEqual(expectedToNotMatch);
+          expect(myPayload.testTypes[0].certificateNumber).toEqual("");
+        });
+      });
+      describe("with existing certificateNumber field", () => {
+        it("should NOT modify the input to add a certificateNumber field", () => {
+          // @ts-ignore
+          const srv = new TestResultsService(null);
+
+          const myPayload = {
+            testStatus: TEST_STATUS.SUBMITTED,
+            testTypes: [
+              {
+                testTypeId: "39", // an LEC test type
+                certificateNumber: "abc123"
+              }
+            ]
+          };
+          const expectedOutput = cloneDeep(myPayload);
+          srv.addMissingRequiredCertificateNumberOnLec(myPayload);
+          expect(myPayload).toEqual(expectedOutput);
+        });
+      });
+    });
+    describe("with non-LEC test type", () => {
+      it("should NOT modify the input", () => {
+        // @ts-ignore
+        const srv = new TestResultsService(null);
+
+        const myPayload = {
+          testStatus: TEST_STATUS.SUBMITTED,
+          testTypes: [
+            {
+              testTypeId: "11", // an non-LEC test type
+              certificateNumber: "abc123"
+            }
+          ]
+        };
+        const expectedOutput = cloneDeep(myPayload);
+        srv.addMissingRequiredCertificateNumberOnLec(myPayload);
+        expect(expectedOutput).toEqual(myPayload);
       });
     });
   });
