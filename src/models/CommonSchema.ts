4a685c312655971ccabd25923861d34d1de1f91a
oalexandru <oalexandru@deloittece.com>
2020-02-20 15:39:06 +0200

    Changed validations for fuel type and emission standards

--- src/models/CommonSchema.ts
+++ src/models/CommonSchema.ts
@@ -34,8 +34,8 @@ export const testTypesCommonSchema = {
         code: Joi.any().only(["p", "m", "g"]),
         description: Joi.any().only(["particulate trap", "modification or change of engine", "gas engine"])
     }).allow(null),
-    emissionStandard: Joi.any().only(["0.16 g/kWh Euro 3 PM", "0.08 g/kWh Euro 3 PM", "0.03 g/kWh Euro 4 PM"]).allow(null),
-    fuelType: Joi.any().only(["diesel", "gas", "petrol"]).allow(null),
+    emissionStandard: Joi.any().only(["0.10 g/kWh Euro 3 PM", "0.03 g/kWh Euro IV PM", "Euro 3", "Euro 4", "Euro 6", "Euro VI", "Full Electric"]).allow(null),
+    fuelType: Joi.any().only(["diesel", "gas-cng", "gas-lng", "gas-lpg", "petrol", "fuel cell", "full electric"]).allow(null),
     particulateTrapSerialNumber: Joi.string().max(100).allow(null),
     smokeTestKLimitApplied: Joi.string().max(100).allow(null),
     modificationTypeUsed: Joi.string().max(100).allow(null),
