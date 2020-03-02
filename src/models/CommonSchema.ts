4dae2723620b91fa54a8a79886eacefa708e0c88
agoncearenco <50991726+agoncearenco@users.noreply.github.com>
2020-03-02 15:37:16 +0000

    CVSB-12530 Added vehicleClass validation values (#152)

--- src/models/CommonSchema.ts
+++ src/models/CommonSchema.ts
@@ -56,8 +56,22 @@ export const testResultsCommonSchema = {
     testEndTimestamp: Joi.date().iso().required(),
     testStatus: Joi.any().only(["submitted", "cancelled"]).required(),
     vehicleClass: Joi.object().keys({
-        code: Joi.any().only(["1", "2", "3", "n", "t", "l", "s", "v"]).required(),
-        description: Joi.any().only(["motorbikes over 200cc or with a sidecar", "not applicable", "small psv (ie: less than or equal to 22 seats)", "motorbikes up to 200cc", "trailer", "large psv(ie: greater than 23 seats)", "3 wheelers", "heavy goods vehicle"]).required()
+        code: Joi.any().only(["1", "2", "3", "n", "s", "t", "l", "v", "4", "5", "7", "p", "u"]).required(),
+        description: Joi.any().only([
+            "motorbikes up to 200cc",
+            "motorbikes over 200cc or with a sidecar",
+            "3 wheelers",
+            "not applicable",
+            "small psv (ie: less than or equal to 22 seats)",
+            "trailer",
+            "large psv(ie: greater than 23 seats)",
+            "heavy goods vehicle",
+            "MOT class 4",
+            "MOT class 5",
+            "MOT class 7",
+            "PSV of unknown or unspecified size",
+            "Not Known"])
+            .required()
     }).required(),
     vehicleType: Joi.any().only(["psv", "hgv", "trl"]).required(),
     noOfAxles: Joi.number().max(99).required(),
