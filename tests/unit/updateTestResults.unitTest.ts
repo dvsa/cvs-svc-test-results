bead44d32d5bf78515621de67688586e96a93f5b
Catalin-Andrei Stancu <castancu@deloittece.com>
2020-03-30 11:24:39 +0300

    removed unnecessary line

--- tests/unit/updateTestResults.unitTest.ts
+++ tests/unit/updateTestResults.unitTest.ts
@@ -20,8 +20,6 @@ describe("updateTestResults", () => {
         });
         testResultsService = new TestResultsService(new MockTestResultsDAO());
         testToUpdate = cloneDeep(testResultsMockDB[30]);
-        testToUpdate.countryOfRegistration = "gb";
-        delete testToUpdate.testTypes[0].testTypeClassification;
     });
 
     afterEach(() => {
