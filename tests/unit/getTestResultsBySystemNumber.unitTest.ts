7353be12b3e70d3b6b84c708d6850dfae8b4b52e
Catalin-Andrei Stancu <castancu@deloittece.com>
2020-04-13 13:55:42 +0300

    fixed tests after rebase

--- tests/unit/getTestResultsBySystemNumber.unitTest.ts
+++ tests/unit/getTestResultsBySystemNumber.unitTest.ts
@@ -144,37 +144,6 @@ describe("getTestResults", () => {
                   });
             });
         });
-
-        context("when the testVersion filter is ALL", () => {
-            it("should return all test-results (without testVersion, testVersion=current/archived)", () => {
-                const firstTest = testResultsMockDB[0];
-                const secondTest = testResultsMockDB[0];
-                firstTest.testVersion = "current";
-                secondTest.testVersion = "archived";
-                const testResults = Array.of(firstTest, secondTest);
-
-                MockTestResultsDAO = jest.fn().mockImplementation(() => {
-                    return {
-                        getBySystemNumber: () => {
-                            return Promise.resolve({
-                                Items: testResults,
-                                Count: 2
-                            });
-                        }
-                    };
-                });
-
-                testResultsService = new TestResultsService(new MockTestResultsDAO());
-                return testResultsService.getTestResults({ systemNumber: "1111", testVersion: TEST_VERSION.ALL})
-                  .then((returnedRecords: any) => {
-                      expect(returnedRecords).not.toEqual(undefined);
-                      expect(returnedRecords).not.toEqual({});
-                      expect(returnedRecords.length).toEqual(2);
-                      expect(JSON.stringify(returnedRecords[0])).toEqual(JSON.stringify(firstTest));
-                      expect(JSON.stringify(returnedRecords[1])).toEqual(JSON.stringify(secondTest));
-                  });
-            });
-        });
     });
 
     context("when db returns undefined data", () => {
