4093a6ec22b5f4b93ce31fc5f60c02adb6301c67
Umer Jaffer <ujaffer@deloitte.co.uk>
2020-11-16 15:33:47 +0000

    chore(cvsb-18734): general code refactor for test result service

--- tests/unit/resultPayload.unitTest.ts
+++ tests/unit/resultPayload.unitTest.ts
@@ -2,21 +2,32 @@ import { ValidationUtil } from "../../src/utils/validationUtil";
 import * as testResultsMockDB from "../resources/test-results.json";
 
 describe("ValidationUtil", () => {
-    context("calling fieldsNullWhenDeficiencyCategoryIsOtherThanAdvisoryresultsPayload having defects other than advisory", () => {
-        it("should add missing fields to defects", () => {
-            const mockData = testResultsMockDB[4];
+  context(
+    "calling fieldsNullWhenDeficiencyCategoryIsOtherThanAdvisoryresultsPayload having defects other than advisory",
+    () => {
+      it("should add missing fields to defects", () => {
+        const mockData = testResultsMockDB[4];
 
-            const result = ValidationUtil.fieldsNullWhenDeficiencyCategoryIsOtherThanAdvisory(mockData);
-            expect(result).toContain("are null for a defect with deficiency category other than advisory");
-        });
-    });
+        const result = ValidationUtil.fieldsNullWhenDeficiencyCategoryIsOtherThanAdvisory(
+          mockData
+        );
+        expect(result).toContain(
+          "are null for a defect with deficiency category other than advisory"
+        );
+      });
+    }
+  );
 
-    context("calling reasonForAbandoningPresentOnAllAbandonedTests with abandoned testTypes", () => {
-        it("should return whether all have reasonForAbandoning or not", () => {
-            const mockData = testResultsMockDB[5];
-            const result = ValidationUtil.reasonForAbandoningPresentOnAllAbandonedTests(mockData);
-            expect(result).toEqual(false);
-        });
-    });
+  context(
+    "calling reasonForAbandoningPresentOnAllAbandonedTests with abandoned testTypes",
+    () => {
+      it("should return whether all have reasonForAbandoning or not", () => {
+        const mockData = testResultsMockDB[5];
+        const result = ValidationUtil.reasonForAbandoningPresentOnAllAbandonedTests(
+          mockData
+        );
+        expect(result).toEqual(false);
+      });
+    }
+  );
 });
-
