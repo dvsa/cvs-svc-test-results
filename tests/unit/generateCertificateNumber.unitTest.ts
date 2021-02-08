uk-gov-mirror/dvsa.cvs-svc-test-results

chore(cvsb-18782): refactor insertTestResult

tests/unit/generateCertificateNumber.unitTest.ts
@@ -1,38 +1,20 @@
-import { TestResultsService } from "../../src/services/TestResultsService";
-import fs from "fs";
-import path from "path";
 import {cloneDeep} from "lodash";
+import { VehicleTestController } from "../../src/handlers/VehicleTestController";
+import * as testResultsPostMock from "../resources/test-results-post.json";
+import { ITestResultPayload } from "../../src/models";
 
-describe("TestResultsService calling generateExpiryDate", () => {
-    let testResultsService: TestResultsService | any;
-    let MockTestResultsDAO: jest.Mock;
-    let testResultsMockDB: any;
-    let testResultsPostMock: any;
-
-    beforeEach(() => {
-        testResultsMockDB = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/test-results.json"), "utf8"));
-        testResultsPostMock = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/test-results-post.json"), "utf8"));
-        MockTestResultsDAO = jest.fn().mockImplementation(() => {
-            return {};
-        });
-        testResultsService = new TestResultsService(new MockTestResultsDAO());
-    });
-
-    afterEach(() => {
-        testResultsMockDB = null;
-        testResultsService = null;
-        MockTestResultsDAO.mockReset();
-    });
+describe("VehicleTestController calling AssignCertificateNumberToTestTypes", () => {
 
     context("when inserting a testResult that is a vehicleType of hgv or trl and it contains at least one Roadworthiness test type and the test result on the Roadworthiness test type is pass", () => {
         it("then a testNumber is generated and the inserted test result should set the testNumber as the certificateNumber.", () => {
-            const testPassedResultWithRoadworthinessTestType = cloneDeep(testResultsPostMock[6]);
+            const testPassedResultWithRoadworthinessTestType = cloneDeep(testResultsPostMock[6] as ITestResultPayload);
             testPassedResultWithRoadworthinessTestType.testTypes[0].testTypeId = "122";
             testPassedResultWithRoadworthinessTestType.testTypes[0].testNumber = "W01A00209";
             testPassedResultWithRoadworthinessTestType.testTypes[0].testResult = "pass";
 
             expect.assertions(2);
-            const updatedResult =  testResultsService.generateCertificateNumber(testPassedResultWithRoadworthinessTestType);
+            // @ts-ignore
+            const updatedResult =  VehicleTestController.AssignCertificateNumberToTestTypes(testPassedResultWithRoadworthinessTestType);
             expect(updatedResult.testTypes[0].testTypeId).toEqual("122");
             expect(updatedResult.testTypes[0].certificateNumber).toEqual("W01A00209");
         });
@@ -40,13 +22,14 @@ describe("TestResultsService calling generateExpiryDate", () => {
 
     context("when inserting a testResult that is a vehicleType of trl and it contains at least one Roadworthiness test type and the test result on the Roadworthiness test type is pass", () => {
         it("then a testNumber is generated and the inserted test result should set the testNumber as the certificateNumber.", () => {
-            const testPassedResultWithRoadworthinessTestType = cloneDeep(testResultsPostMock[6]);
+            const testPassedResultWithRoadworthinessTestType = cloneDeep(testResultsPostMock[6] as ITestResultPayload);
             testPassedResultWithRoadworthinessTestType.testTypes[0].testTypeId = "91";
             testPassedResultWithRoadworthinessTestType.testTypes[0].testNumber = "W01A00209";
             testPassedResultWithRoadworthinessTestType.testTypes[0].testResult = "pass";
 
             expect.assertions(2);
-            const updatedResult =  testResultsService.generateCertificateNumber(testPassedResultWithRoadworthinessTestType);
+            // @ts-ignore
+            const updatedResult =  VehicleTestController.AssignCertificateNumberToTestTypes(testPassedResultWithRoadworthinessTestType);
             expect(updatedResult.testTypes[0].testTypeId).toEqual("91");
             expect(updatedResult.testTypes[0].certificateNumber).toEqual("W01A00209");
         });

dvsa/cvs-svc-test-results

tests/unit/generateCertificateNumber.unitTest.ts
@@ -4,6 +4,7 @@ import testResults from "../resources/test-results.json";
 import postTestResults from "../resources/test-results-post.json";
 import { TEST_TYPE_CLASSIFICATION, VEHICLE_TYPES, TEST_RESULT } from "../../src/assets/Enums";
 import { ITestResultPayload } from "../../src/models/ITestResultPayload";
+import { VehicleTestController } from "../../src/handlers/VehicleTestController";
 
 describe("TestResultsService calling generateExpiryDate", () => {
     let testResultsService: TestResultsService;
@@ -71,7 +72,8 @@ describe("TestResultsService calling generateExpiryDate", () => {
             baseTestResult.testTypes[0].testTypeId = testTypeId;
 
             expect.assertions(2);
-            const updatedResult =  testResultsService.generateCertificateNumber(baseTestResult);
+            // @ts-ignore
+            const updatedResult =  VehicleTestController.AssignCertificateNumberToTestTypes(baseTestResult);
             expect(updatedResult.testTypes[0].certificateNumber === null).not.toBe(shouldSetCertificateNumber);
             expect(updatedResult.testTypes[0].certificateNumber === "W01A00209").toBe(shouldSetCertificateNumber);
 