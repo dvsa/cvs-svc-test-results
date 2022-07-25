880f2b0fd52670b2bfd4328e9ed4a3aa86b1d3ee
kpearmanbjss <kris.pearman@bjss.com>
2022-07-25 10:27:17 +0100

    feat(cb2-3949): added additional null validation to be in line with dynamo settings

--- src/models/validators/TestResultsSchemaHGVCancelled.ts
+++ src/models/validators/TestResultsSchemaHGVCancelled.ts
@@ -1,34 +1,64 @@
 import * as Joi from "joi";
-import {defectsCommonSchema, testTypesCommonSchema, testResultsCommonSchema} from "./CommonSchema";
+import {
+  defectsCommonSchema,
+  testTypesCommonSchema,
+  testResultsCommonSchema,
+} from "./CommonSchema";
 
 const defectsSchema = defectsCommonSchema.keys({
-    additionalInformation: Joi.object().keys({
-        location: Joi.object().keys({
-            vertical: Joi.any().only(["upper", "lower"]).required().allow(null),
-            horizontal: Joi.any().only(["inner", "outer"]).required().allow(null),
-            lateral: Joi.any().only(["nearside", "centre", "offside"]).required().allow(null),
-            longitudinal: Joi.any().only(["front", "rear"]).required().allow(null),
-            rowNumber: Joi.number().max(20).required().allow(null),
-            seatNumber: Joi.number().max(6).required().allow(null),
-            axleNumber: Joi.number().max(10).required().allow(null)
-        }).required().allow(null),
-        notes: Joi.string().max(500).required().allow("", null)
-    })
+  additionalInformation: Joi.object().keys({
+    location: Joi.object()
+      .keys({
+        vertical: Joi.any().only(["upper", "lower"]).required().allow(null),
+        horizontal: Joi.any().only(["inner", "outer"]).required().allow(null),
+        lateral: Joi.any()
+          .only(["nearside", "centre", "offside"])
+          .required()
+          .allow(null),
+        longitudinal: Joi.any().only(["front", "rear"]).required().allow(null),
+        rowNumber: Joi.number().max(20).required().allow(null),
+        seatNumber: Joi.number().max(6).required().allow(null),
+        axleNumber: Joi.number().max(10).required().allow(null),
+      })
+      .required()
+      .allow(null),
+    notes: Joi.string().max(500).required().allow("", null),
+  }),
 });
 
 const testTypesSchema = testTypesCommonSchema.keys({
-    testResult: Joi.any().only(["fail", "pass", "prs", "abandoned"]).required().allow(null),
-    testTypeEndTimestamp: Joi.date().iso().required().allow(null),
-    defects: Joi.array().items(defectsSchema).required()
+  testResult: Joi.any()
+    .only(["fail", "pass", "prs", "abandoned"])
+    .required()
+    .allow(null),
+  testTypeEndTimestamp: Joi.date().iso().required().allow(null),
+  defects: Joi.array().items(defectsSchema).required(),
 });
 
 export const hgvCancelled = testResultsCommonSchema.keys({
-    vrm: Joi.string().alphanum().min(1).max(8).required(),
-    reasonForCancellation: Joi.string().max(500).required().allow(""),
-    odometerReading: Joi.number().required().allow(null),
-    odometerReadingUnits: Joi.any().only(["kilometres", "miles"]).required().allow(null),
-    countryOfRegistration: Joi.string().required().allow("", null),
-    vehicleConfiguration: Joi.any().only(["rigid", "articulated", "centre axle drawbar", "semi-car transporter", "semi-trailer", "low loader", "other", "drawbar", "four-in-line", "dolly", "full drawbar"]).required(),
-    testTypes: Joi.array().items(testTypesSchema).required(),
-    regnDate: Joi.string().allow("", null)
+  vrm: Joi.string().alphanum().min(1).max(8).required(),
+  reasonForCancellation: Joi.string().max(500).required().allow("", null),
+  odometerReading: Joi.number().required().allow(null),
+  odometerReadingUnits: Joi.any()
+    .only(["kilometres", "miles"])
+    .required()
+    .allow(null),
+  countryOfRegistration: Joi.string().required().allow("", null),
+  vehicleConfiguration: Joi.any()
+    .only([
+      "rigid",
+      "articulated",
+      "centre axle drawbar",
+      "semi-car transporter",
+      "semi-trailer",
+      "low loader",
+      "other",
+      "drawbar",
+      "four-in-line",
+      "dolly",
+      "full drawbar",
+    ])
+    .required(),
+  testTypes: Joi.array().items(testTypesSchema).required(),
+  regnDate: Joi.string().allow("", null),
 });
--- src/models/validators/TestResultsSchemaTRLCancelled.ts
+++ src/models/validators/TestResultsSchemaTRLCancelled.ts
@@ -1,32 +1,59 @@
 import * as Joi from "joi";
-import {defectsCommonSchema, testTypesCommonSchema, testResultsCommonSchema} from "./CommonSchema";
+import {
+  defectsCommonSchema,
+  testTypesCommonSchema,
+  testResultsCommonSchema,
+} from "./CommonSchema";
 
 const defectsSchema = defectsCommonSchema.keys({
-    additionalInformation: Joi.object().keys({
-        location: Joi.object().keys({
-            vertical: Joi.any().only(["upper", "lower"]).required().allow(null),
-            horizontal: Joi.any().only(["inner", "outer"]).required().allow(null),
-            lateral: Joi.any().only(["nearside", "centre", "offside"]).required().allow(null),
-            longitudinal: Joi.any().only(["front", "rear"]).required().allow(null),
-            rowNumber: Joi.number().max(20).required().allow(null),
-            seatNumber: Joi.number().max(6).required().allow(null),
-            axleNumber: Joi.number().max(10).required().allow(null)
-        }).required().allow(null),
-        notes: Joi.string().max(500).required().allow("", null)
-    })
+  additionalInformation: Joi.object().keys({
+    location: Joi.object()
+      .keys({
+        vertical: Joi.any().only(["upper", "lower"]).required().allow(null),
+        horizontal: Joi.any().only(["inner", "outer"]).required().allow(null),
+        lateral: Joi.any()
+          .only(["nearside", "centre", "offside"])
+          .required()
+          .allow(null),
+        longitudinal: Joi.any().only(["front", "rear"]).required().allow(null),
+        rowNumber: Joi.number().max(20).required().allow(null),
+        seatNumber: Joi.number().max(6).required().allow(null),
+        axleNumber: Joi.number().max(10).required().allow(null),
+      })
+      .required()
+      .allow(null),
+    notes: Joi.string().max(500).required().allow("", null),
+  }),
 });
 
 const testTypesSchema = testTypesCommonSchema.keys({
-    testTypeEndTimestamp: Joi.date().iso().required().allow(null),
-    testResult: Joi.any().only(["fail", "pass", "prs", "abandoned"]).required().allow(null),
-    defects: Joi.array().items(defectsSchema).required()
+  testTypeEndTimestamp: Joi.date().iso().required().allow(null),
+  testResult: Joi.any()
+    .only(["fail", "pass", "prs", "abandoned"])
+    .required()
+    .allow(null),
+  defects: Joi.array().items(defectsSchema).required(),
 });
 
 export const trlCancelled = testResultsCommonSchema.keys({
-    reasonForCancellation: Joi.string().max(500).required().allow(""),
-    countryOfRegistration: Joi.string().required().allow("", null),
-    vehicleConfiguration: Joi.any().only(["rigid", "articulated", "centre axle drawbar", "semi-car transporter", "semi-trailer", "low loader", "other", "drawbar", "four-in-line", "dolly", "full drawbar"]).required(),
-    trailerId: Joi.string().required(),
-    testTypes: Joi.array().items(testTypesSchema).required(),
-    firstUseDate: Joi.string().allow("", null)
+  reasonForCancellation: Joi.string().max(500).required().allow("", null),
+  countryOfRegistration: Joi.string().required().allow("", null),
+  vehicleConfiguration: Joi.any()
+    .only([
+      "rigid",
+      "articulated",
+      "centre axle drawbar",
+      "semi-car transporter",
+      "semi-trailer",
+      "low loader",
+      "other",
+      "drawbar",
+      "four-in-line",
+      "dolly",
+      "full drawbar",
+    ])
+    .required(),
+  trailerId: Joi.string().required(),
+  testTypes: Joi.array().items(testTypesSchema).required(),
+  firstUseDate: Joi.string().allow("", null),
 });
