6b7411ab4b96abf5c65887f253ddc410136862ed
Hazel <114985934+HazelWilderspin@users.noreply.github.com>
2023-02-20 09:54:18 +0000

    feat(cb2-7543): amend hgv type approved dba (#321)

--- src/assets/Enums.ts
+++ src/assets/Enums.ts
@@ -578,6 +578,8 @@ export const TEST_TYPES_GROUP4_DESK_BASED_TEST: string[] = [
   '424',
   '425',
 ];
+
+// ADR: ADRIIIa
 export const TEST_TYPES_GROUP5_DESK_BASED_TEST: string[] = ['441', '442'];
 
 export const TEST_TYPES = {
