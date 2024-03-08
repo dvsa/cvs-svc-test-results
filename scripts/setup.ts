6b5dd5c857ba82f3e4ad3758c7b65811e570c996
Shivangi Das <shivangi.das7@gmail.com>
2024-03-08 17:12:12 +0000

    test(cb2-11221): update serverless

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 
 // We hook to serverless offline when firing its process
-const SERVER_OK = `Offline [HTTP] listening on http://localhost:3006`;
+const SERVER_OK = `Server ready: http://localhost:3006 🚀`;
 // Serverless fires a local dynamo-db instance which is killed once the parent process is terminated
 // the current serverless script checks whether a local instance is running but does not error when binding fails
 // we force throwing an error so we always start from a clean slate if java.io.IOException: Failed to bind to 0.0.0.0/0.0.0.0:8006
@@ -10,17 +10,20 @@ const DYNAMO_LOCAL_ERROR_THREAD = `Exception in thread "main"`;
 const setupServer = (process: any) =>
   new Promise((resolve, reject) => {
     process.stdout.setEncoding('utf-8').on('data', (stream: any) => {
-      console.log(stream);
+      console.log("stdout", stream);
       if (stream.includes(SERVER_OK)) {
         resolve(process);
       }
     });
 
     process.stderr.setEncoding('utf-8').on('data', (stream: any) => {
-      // if (stream.includes(DYNAMO_LOCAL_ERROR_THREAD)) {
-      //   throw new Error('Internal Java process crashed');
-      // }
-      // reject(stream);
+      console.log("stderr", stream);
+      if (stream.includes(DYNAMO_LOCAL_ERROR_THREAD)) {
+        throw new Error('Internal Java process crashed');
+      }
+      else if (stream.includes(SERVER_OK)) {
+        resolve(process);
+      }
     });
 
     process.on('exit', (code: any, signal: any) => {
