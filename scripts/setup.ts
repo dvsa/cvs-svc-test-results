c9b22c6e951dea7c0e4af1890586d12c4f7db460
Shivangi Das <shivangi.das7@gmail.com>
2024-03-11 12:30:09 +0000

    chore(11221): downgrading serverless

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 
 // We hook to serverless offline when firing its process
-const SERVER_OK = `Server ready: http://localhost:3006 🚀`;
+const SERVER_OK = `Enter "rp" to replay the last request`;
 // Serverless fires a local dynamo-db instance which is killed once the parent process is terminated
 // the current serverless script checks whether a local instance is running but does not error when binding fails
 // we force throwing an error so we always start from a clean slate if java.io.IOException: Failed to bind to 0.0.0.0/0.0.0.0:8006
@@ -21,9 +21,7 @@ const setupServer = (process: any) =>
       if (stream.includes(DYNAMO_LOCAL_ERROR_THREAD)) {
         throw new Error('Internal Java process crashed');
       }
-      else if (stream.includes(SERVER_OK)) {
-        resolve(process);
-      }
+      reject(stream);
     });
 
     process.on('exit', (code: any, signal: any) => {
