4b3823298fa6a01c1c8aeab1a7b9d315c65a3f55
Owen Corrigan (BJSS, VTM) <122352145+owen-corrigan-bjss@users.noreply.github.com>
2024-04-12 14:55:47 +0100

    chore(cb2-11271): linting

--- src/utils/validateInvocationResponse.ts
+++ src/utils/validateInvocationResponse.ts
@@ -18,9 +18,9 @@ export const validateInvocationResponse = (response: any) => {
   let payload: any;
 
   try {
-    const string = Buffer.from(response.Payload).toString()
-    console.log(string)
-    console.log(typeof string)
+    const string = Buffer.from(response.Payload).toString();
+    console.log(string);
+    console.log(typeof string);
     payload = JSON.parse(string);
   } catch (error) {
     console.log('validateInvocationResponse response parse error', response);
