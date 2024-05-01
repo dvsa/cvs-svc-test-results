e9563a326c00cb0d8c1ab65f7f27e5c684370a62
Owen Corrigan (BJSS, VTM) <122352145+owen-corrigan-bjss@users.noreply.github.com>
2024-05-01 14:47:36 +0100

    feat(cb2-11271): remove undefined values

--- src/models/TestResultsDAO.ts
+++ src/models/TestResultsDAO.ts
@@ -26,7 +26,10 @@ export class TestResultsDAO {
   constructor() {
     const config = Configuration.getInstance().getDynamoDBConfig();
     const translateConfig = {
-      marshallOptions: { convertEmptyValues: true, removeUndefinedValues: true },
+      marshallOptions: {
+        convertEmptyValues: true,
+        removeUndefinedValues: true,
+      },
     };
 
     this.tableName = config.table;
