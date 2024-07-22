06c1ae38ae87589fbe9fde3bd37f07d0d9df1d73
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2024-07-22 16:02:16 +0100

    feat(cb2-13154): linting

--- tests/integration/postTestResults.intTest.ts
+++ tests/integration/postTestResults.intTest.ts
@@ -7,15 +7,10 @@ const url = 'http://localhost:3006/';
 const request = supertest(url);
 
 describe('postTestResults', () => {
-  // let testResult: ITestResultPayload;
-  // beforeEach(() => {
-  //   testResult = testResultsPostMock[15] as unknown as ITestResultPayload;
-  //   jest.resetAllMocks();
-  // });
-
   context('when submitting a test result with central docs', () => {
     it('should return 400 when central docs are required but missing', async () => {
-      const testResult = testResultsPostMock[15] as unknown as ITestResultPayload;
+      const testResult =
+        testResultsPostMock[15] as unknown as ITestResultPayload;
 
       testResult.testTypes[0].testTypeId = CENTRAL_DOCS_TEST_TYPES[3];
       delete testResult.testTypes[0].centralDocs;
@@ -29,7 +24,8 @@ describe('postTestResults', () => {
 
   context('when submitting an invalid test result', () => {
     it('should return 400 for missing required fields', async () => {
-      const testResult = testResultsPostMock[10] as unknown as ITestResultPayload;
+      const testResult =
+        testResultsPostMock[10] as unknown as ITestResultPayload;
       delete testResult.testResultId;
       const res = await request.post('test-results').send(testResult);
 
