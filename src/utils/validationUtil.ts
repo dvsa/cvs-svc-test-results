846628f87af180043d86581b25933c359c409a3b
Craig Broady <Craig.Broady@dvsa.gov.uk>
2024-11-14 10:33:17 +0000

    fix(cb2-12681): corrected validation util list

--- src/utils/validationUtil.ts
+++ src/utils/validationUtil.ts
@@ -6,7 +6,8 @@ import {
   CENTRAL_DOCS_TEST,
   HGV_TRL_RWT_TEST,
   IVA_TEST,
-  LEC_TEST, MSVA_TEST,
+  LEC_TEST,
+  MSVA_TEST,
   TIR_TEST,
 } from '@dvsa/cvs-microservice-common/classes/testTypes/Constants';
 import {
@@ -288,7 +289,10 @@ export class ValidationUtil {
 
   private static isIvaTest(tests: TestTypeSchema[]): boolean {
     return tests.every((test: TestTypeSchema) =>
-      TestTypeHelper.validateTestTypeIdInLists([IVA_TEST, MSVA_TEST], test.testTypeId),
+      TestTypeHelper.validateTestTypeIdInLists(
+        [IVA_TEST, MSVA_TEST],
+        test.testTypeId,
+      ),
     );
   }
 
